3d1c2109e2bd50bf1879c7a5147a1d130e781324
Wilfrido Bonett <wbonett10@gmail.com>
2020-04-09 14:05:41 -0500

    I add services and get all data

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,29 @@
-const BASE_URL = 'https://jsonplaceholder.typicode.com/';
+const BASE_URL = 'https://jsonplaceholder.typicode.com';
 
-const fetchPosts = () => {
-    axios.get(`${BASE_URL}posts`)
-        .then(response => {
-            const posts = response.data;
-            console.log(`GET list posts`, posts);
-            // append to DOM
-            // appendToDOM(posts);
-        })
-        .catch(error => console.error(error));
+const addTestimonialToDOM = (postList, userList) => {
+    console.log(`GET: Here's the lists`, postList, userList);
+}
+
+const getUsers = async () => {
+    try {
+        const response = await axios.get(`${BASE_URL}/users`);
+        return response.data;
+    } catch (e) {
+        console.error(e);
+    }
+}
+
+const getPosts = async () => {
+    try {
+        const response = await axios.get(`${BASE_URL}/posts`);
+        return response.data;
+    } catch (e) {
+        console.error(e);
+    }
 };
 
-fetchPosts();
+const main = async () => {
+    addTestimonialToDOM(await getPosts(), await getUsers());
+};
+
+main();
