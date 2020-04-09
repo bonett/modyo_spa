e285232b2d3ee61a5c782140cfb53634e183fb96
Wilfrido Bonett <wbonett10@gmail.com>
2020-04-09 16:20:26 -0500

    service is working

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,48 @@
+/* EndPoint Base */
 const BASE_URL = 'https://jsonplaceholder.typicode.com';
 
-const addTestimonialToDOM = (postList, userList) => {
-    console.log(`GET: Here's the lists`, postList, userList);
+/**
+ * It should merge two collections through comparison param1 and param2
+ * 
+ * @param {collection1} Array
+ * @param {collection2} Array
+ * @param {param1} string
+ * @param {param2} string
+ * @return Array 
+ * 
+ */
+const handlerReduceList = (collection1, collection2, param1, param2) => {
+    return collection1.map(a => ({
+        ...collection2.find((b) => (b[param1] === a[param2]) && b),
+        ...a
+    }));
 }
 
+/**
+ * It should validate and get a testimonial data by users to render on screen
+ * 
+ * @param {postList} Array
+ * @param {userList} Array
+ * 
+ */
+const addTestimonialToDOM = (postList, userList) => {
+
+    let testimonials = handlerReduceList(postList, userList, 'userId', 'id');
+
+    testimonials = _.sampleSize(testimonials, 5); // It should take 5 positions Randomly - Lodash
+
+    console.log(
+        testimonials
+    )
+}
+
+/**
+ *  Get All Users
+ * 
+ * @param null
+ * @returns {response.data} Array
+ * 
+ */
 const getUsers = async () => {
     try {
         const response = await axios.get(`${BASE_URL}/users`);
@@ -13,6 +52,13 @@ const getUsers = async () => {
     }
 }
 
+/**
+ *  Get All Post
+ * 
+ * @param null
+ * @returns {response.data} Array
+ * 
+ */
 const getPosts = async () => {
     try {
         const response = await axios.get(`${BASE_URL}/posts`);
@@ -22,8 +68,16 @@ const getPosts = async () => {
     }
 };
 
+/**
+ *  It should call addTestimonialToDOM method to render data on screen
+ * 
+ * @param null
+ * 
+ */
 const main = async () => {
     addTestimonialToDOM(await getPosts(), await getUsers());
 };
+
+/* Initialize script */
 
 main();
